c573f383aa2884cf44b0e756ceb16a34397b2d40
jwtr13 <jackwatson1995@live.com>
2017-03-29 07:46:11 +0100

    Drag/Drop Scripts
    These scripts handle dragging and dropping icons within the game.

---
+++ DragScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    //Need a location to return the dragged gameObject to when the user stops dragging
+    //This will be overwritten if the item is over a valid drop area
+    public Transform returnLocation = null;
+
+    //Begin Drag
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Get the location of the original area that the dragged item was
+        returnLocation = this.transform.parent;
+        //Remove the game object as a child from the area it is in, and make it a child of the main canvas
+        //This will let it find a new parent, when the item is dropped
+        this.transform.SetParent(GameObject.FindWithTag("MainCanvas").transform);
+        //Turn off the raycast blocking, so that the raycast can be used to find what the item is above
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    //Drag
+    public void OnDrag(PointerEventData eventData)
+    {
+        //make the dragged item's transform.position the same as the mouse position
+        this.transform.position = eventData.position;
+    }
+
+    //End Drag
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Return the item to it's original location
+        this.transform.SetParent(returnLocation);
+        //Turn raycast blocking back on
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
+
---
+++ DropZoneScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZoneScript : MonoBehaviour, IDropHandler
+{
+
+    //int to see how many children a gameobject has
+    int children;
+    //int to set maximum number of children a gameobject can have
+    public int maxChildren;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //find out how many children the potential dropzone has
+        children = this.transform.childCount;
+        //Find the item to be dropped, and change the returnLocation to be the Drop Area
+        DragScript drag = eventData.pointerDrag.GetComponent<DragScript>();
+        //make sure script is there, and that the maximum number of children has not been reached
+        if (drag != null && children < maxChildren)
+        {
+            //make the item being dragged a child of the drop area
+            drag.returnLocation = this.transform;
+        }
+    }
+}
+
